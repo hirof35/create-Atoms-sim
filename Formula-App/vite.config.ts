5dbac93537d2977ca53f0720136324255bb51bbb
hirosi osugi <good59548@gmail.com>
2026-06-02 11:33:58 +0900

    Delete Formula-App directory

--- Formula-App/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Formula-app/', // ★ここを追加！作成したGitHubのリポジトリ名と同じにします
-})
